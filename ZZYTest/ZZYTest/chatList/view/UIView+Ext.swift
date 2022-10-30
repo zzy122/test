1611f2f47af222efda777b2d137f76879f8bae7e
zzy122 <1272046491@qq.com>
2022-10-30 16:01:34 +0800

    m

--- ZZYTest/ZZYTest/chatList/view/UIView+Ext.swift
+++ ZZYTest/ZZYTest/chatList/view/UIView+Ext.swift
@@ -3,6 +3,7 @@ import UIKit
 
 extension UIView {
     func createCorneradius(cornerWidth:CGFloat, corners:UIRectCorner) {
+        
         let layer = CAShapeLayer()
         layer.path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerWidth, height: cornerWidth)).cgPath
         self.layer.mask = layer
